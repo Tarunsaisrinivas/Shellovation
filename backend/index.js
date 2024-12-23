756deb2029ea1094b1ab85f3a3167a1b9c3e4aa4
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2024-12-23 16:33:26 +0530

    small change

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,9 @@ mongoose
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-
+app.use("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
